3a1a069b7526d922c8f50249fa83c3a731781609
Kaye Engbino <engbinop@gmail.com>
2020-10-22 23:41:07 +0800

    Iterative (trailer record)
    The goals scored in a soccer match may be input as a sequence of 1s and 2s terminated by 0. The number 1 would mean that team 1 made a goal and 2 would mean that team 2 made a goal. Thus the sequence 1 2 1 2 1 1 2 0 means that Team 1 made 4 goals and team 2 made 3 goals in all. Write a program that would input such sequence and output how many goals each team made and the result of the match (“Team 1 won”, ”Team 2 won”, “Both tied”). Ensure that inputs are 0,1 and 2 only.

---
+++ Iterative (trailer record)/TrailerRecord/src/TrailerRecord.java
@@ -0,0 +1,43 @@
+ import java.util.*;
+ 
+ public class TrailerRecord
+ {
+     public static void main ( String[] args)
+     {
+        int team1 = 0, team2 = 0, goal;
+
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("Goals scored in a soccer match: ");
+         goal = sc.nextInt();
+ 
+        while (goal == 1 || goal == 2)
+        {
+            //Tally scores
+            if (goal == 1)
+                team1++;
+            else if (goal == 2)
+                team2++;
+
+            goal = sc.nextInt();
+        }
+        
+        if (goal == 0)
+        {
+            //Print total goals each team
+            System.out.println("\nTeam 1: " + team1);
+            System.out.println("Team 2: " + team2);
+
+            if (team1 > team2)
+                System.out.println("\nTeam 1 won");
+            else if (team1 < team2)
+                System.out.println("\nTeam 2 won");
+            else 
+                System.out.println("\nBoth tied");
+        }
+        else
+             System.out.println("Invalid input");
+
+         sc.close();
+     }
+ }
