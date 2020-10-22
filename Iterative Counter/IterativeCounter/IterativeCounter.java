c62a1e802ab28ae0962543b5bfbed8ec280b1a8f
Kaye Engbino <engbinop@gmail.com>
2020-10-22 21:26:37 +0800

    Iterative Counter
    Take as input the student’s grades of seven subjects and its corresponding unit taken in a semester. A student is considered failed if the average grade is less than 3.0. Compute the student’s General Point Average (GPA) by multiplying the grade per subject by its corresponding unit, sum up the products and divide the sum by its total units. Print the computed average grade and whether the student “failed” or “passed”.

---
+++ Iterative Counter/IterativeCounter/IterativeCounter.java
@@ -0,0 +1,34 @@
+package IterativeCounter;
+import java.util.*;
+
+public class IterativeCounter
+{
+    public static void main(String[] args) throws Exception 
+    {
+        float grade, sum = 0;
+        int unit, unitsTotal = 0;
+
+        Scanner sc = new Scanner(System.in);    
+
+        for(int i = 0, j = 1; i < 7; i++, j++)
+        {
+            System.out.print("Student grade (" + j + "): "); 
+            grade = sc.nextFloat();
+            
+            System.out.print("Unit for subject " + j + ": ");  
+            unit = sc.nextInt();
+            sum += grade * unit;
+            unitsTotal += unit;  
+        }
+
+        float GPA = sum / unitsTotal;   //Computing GPA
+        
+        System.out.println("GPA: " + GPA);
+        if (GPA < 3.0)   
+            System.out.println("Student failed");
+        else 
+            System.out.println("Student passed");
+
+        sc.close(); 
+    }
+}
